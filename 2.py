2720851564fec1f626613d6d4a4876e048b622e7
MinionPole <91430438+MinionPole@users.noreply.github.com>
2021-11-11 00:47:25 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 import yaml
 import json
-with open("data2.txt", 'r') as yaml_in, open("JJJ.json", "w") as json_out:
+with open("data2.txt", 'r', encoding='utf-8') as yaml_in, open("JJJ.json", "w", encoding='utf-8') as json_out:
     yaml_object = yaml.safe_load(yaml_in)
     json.dump(yaml_object, json_out)
